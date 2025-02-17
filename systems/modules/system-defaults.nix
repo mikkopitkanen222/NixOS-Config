6471acbc3e191e73fe6d7f453f8c4cadf6fb331f
Mikko Pitkänen <mikko.pitkanen.code@pm.me>
2025-02-17 21:26:36 +0200

    Fix defaulting in system defaults

--- systems/modules/system-defaults.nix
+++ systems/modules/system-defaults.nix
@@ -17,28 +17,26 @@ in
     enable = lib.mkEnableOption "default values for basic options set system wide.";
   };
 
-  config = lib.mkIf cfg.enable (
-    lib.mkDefault {
-      # We use flakes for our configurations, so channels are obsolete.
-      nix.settings.experimental-features = "nix-command flakes";
-      nix.channel.enable = false;
+  config = lib.mkIf cfg.enable {
+    # We use flakes for our configurations, so channels are obsolete.
+    nix.settings.experimental-features = lib.mkDefault "nix-command flakes";
+    nix.channel.enable = lib.mkDefault false;
 
-      # Delete old profiles and unreachable objects from the Nix store.
-      # Does not delete the current generation of any profile.
-      nix.gc = {
-        automatic = true;
-        dates = "daily";
-        options = "--delete-older-than 5d";
-      };
+    # Delete old profiles and unreachable objects from the Nix store.
+    # Does not delete the current generation of any profile.
+    nix.gc = {
+      automatic = lib.mkDefault true;
+      dates = lib.mkDefault "daily";
+      options = lib.mkDefault "--delete-older-than 5d";
+    };
 
-      networking.networkmanager.enable = true;
+    networking.networkmanager.enable = lib.mkDefault true;
 
-      # Use the same instance of pkgs for NixOS and home-manager.
-      home-manager.useGlobalPkgs = true;
+    # Use the same instance of pkgs for NixOS and home-manager.
+    home-manager.useGlobalPkgs = lib.mkDefault true;
 
-      # Install packages to /etc/profiles instead of ~/.nix-profile.
-      # Required for 'nixos-rebuild build-vm'.
-      home-manager.useUserPackages = true;
-    }
-  );
+    # Install packages to /etc/profiles instead of ~/.nix-profile.
+    # Required for 'nixos-rebuild build-vm'.
+    home-manager.useUserPackages = lib.mkDefault true;
+  };
 }
