6f63dc523270d5e213d93d18939fbab5ec4f3b09
Mikko Pitkänen <mikko.pitkanen.code@pm.me>
2025-06-02 22:25:16 +0300

    feat(hyprland): add bluetooth

---
+++ systems/main/bluetooth.nix
@@ -0,0 +1,9 @@
+# Configure a Bluetooth client.
+{ pkgs, ... }:
+{
+  # Make sure bluetooth.service is enabled / starts at boot.
+  systemd.services.bluetooth.wantedBy = [ "multi-user.target" ];
+
+  # https://github.com/kaii-lb/overskride
+  environment.systemPackages = [ pkgs.overskride ];
+}
