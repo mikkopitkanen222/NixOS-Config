663fc119a763682cd3024450856002820b0dd7c9
Mikko Pitkänen <mikko.pitkanen.code@pm.me>
2025-06-11 22:30:20 +0300

    feat: add powerline

--- users/desknix-mp/bash.nix
+++ users/desknix-mp/bash.nix
@@ -18,18 +18,31 @@
         ".." = "cd ..";
         "..." = "cd ../..";
       };
-      # PS1: [green]user@host [cyan]pwd [red]git-prompt
-      initExtra = ''
-        . /etc/profiles/per-user/mp/share/bash-completion/completions/git
-        . /etc/profiles/per-user/mp/share/bash-completion/completions/git-prompt.sh
-        export GIT_PS1_SHOWDIRTYSTATE=1
-        PS1='\n\033[38;2;0;255;43m\]\u@\h \033[38;2;0;215;255m\]\w\033[38;2;255;0;78m\]$(__git_ps1 " (%s)")\033[0m\]\n\$\040'
-      '';
     };
 
-    # Render "user@host" part of PS1 with magenta background when inside Nix shell.
-    nix.settings.bash-prompt = ''
-      \n\033[38;2;0;255;43;48;2;102;44;86m\]\u@\h \033[38;2;0;215;255m\]\w\033[38;2;255;0;78m\]$(__git_ps1 " (%s)")\033[0m\]\n\$\040
-    '';
+    programs.powerline-go = {
+      enable = true;
+      settings = {
+        hostname-only-if-ssh = true;
+        numeric-exit-codes = true;
+      };
+      extraUpdatePS1 = ''
+        PS1="\n$PS1"
+      '';
+      newline = true;
+      modules = [
+        "nix-shell"
+        "venv"
+        "user"
+        "host"
+        "ssh"
+        "cwd"
+        "perms"
+        "git"
+        "hg"
+        "jobs"
+        "exit"
+      ];
+    };
   };
 }
