724bbedeb9a1324c6ea2ca65715c6602587b88ce
Mikko Pitkänen <mikko.pitkanen.code@pm.me>
2025-06-23 02:03:11 +0300

    chore: add spotify-player settings

--- users/desknix-mp/spotify-player.nix
+++ users/desknix-mp/spotify-player.nix
@@ -6,6 +6,64 @@
   home-manager.users.mp = {
     programs.spotify-player = {
       enable = true;
+      settings = {
+        theme = "theme";
+        device = {
+          volume = 100;
+          audio_cache = true;
+          autoplay = true;
+        };
+      };
+      themes = [
+        {
+          name = "theme";
+          component_style = {
+            block_title = {
+              fg = "Magenta";
+            };
+            border = { };
+            playback_status = {
+              fg = "Green";
+            };
+            playback_track = {
+              fg = "Cyan";
+              modifiers = [ "Bold" ];
+            };
+            playback_artists = {
+              fg = "Cyan";
+            };
+            playback_album = {
+              fg = "Yellow";
+            };
+            playback_metadata = {
+              fg = "BrightBlack";
+            };
+            playback_progress_bar = {
+              fg = "Green";
+              bg = "Black";
+            };
+            current_playing = {
+              fg = "Green";
+            };
+            page_desc = {
+              fg = "Cyan";
+              modifiers = [ "Bold" ];
+            };
+            table_header = {
+              fg = "Blue";
+            };
+            selection = {
+              modifiers = [
+                "Bold"
+                "Reversed"
+              ];
+            };
+            like = {
+              fg = "Red";
+            };
+          };
+        }
+      ];
     };
   };
 }
