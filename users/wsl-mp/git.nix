6995e9122bee0b4b9e07fbeba5794285b5402213
Mikko Pitkänen <mikko.pitkanen.code@pm.me>
2025-08-21 20:57:04 +0300

    feat(wsl-mp): configure git email using secrets

--- users/wsl-mp/git.nix
+++ users/wsl-mp/git.nix
@@ -1,15 +1,24 @@
 # nixos-config/users/wsl-mp/git.nix
 # Configure Git for user 'mp' on host 'wsl'.
-{ ... }:
+{ config, ... }:
 {
   home-manager.users.mp = {
     programs.git = {
       enable = true;
       userName = "Mikko Pitkänen";
-      userEmail = "change.me@work";
       extraConfig = {
         core.pager = "less -x2";
       };
+      includes = [ { path = config.sops.templates."secret-gitconfig".path; } ];
     };
+  };
+
+  sops.secrets."work_email" = { };
+  sops.templates."secret-gitconfig" = {
+    content = ''
+      [user]
+      email = "${config.sops.placeholder."work_email"}"
+    '';
+    owner = config.users.users.mp.name;
   };
 }
