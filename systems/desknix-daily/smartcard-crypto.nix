2bb4e5dc2dbd0f02fdf870cb63ff3531af7bc06a
Mikko Pitkänen <mikko.pitkanen.code@pm.me>
2025-08-23 16:23:31 +0300

    feat: configure u2f keys declaratively
     * Configure additional GPG settings

--- systems/desknix-daily/smartcard-crypto.nix
+++ systems/desknix-daily/smartcard-crypto.nix
@@ -13,6 +13,10 @@
     enable = true;
     enableSSHSupport = true;
     pinentryPackage = pkgs.pinentry-curses;
+    settings = {
+      default-cache-ttl = 30;
+      max-cache-ttl = 60;
+    };
   };
 
   # Yubikey smartcard mode (CCID) requires the PCSC-Lite daemon.
@@ -28,6 +32,28 @@
         # By disabling the integrated GnuPG support for CCID,
         # GnuPG falls back to using the PCSC-Lite daemon instead.
         scdaemonSettings.disable-ccid = true;
+        # GPG hardening:
+        settings = {
+          personal-cipher-preferences = "AES256 AES192 AES";
+          personal-digest-preferences = "SHA512 SHA384 SHA256";
+          personal-compress-preferences = "ZLIB BZIP2 ZIP Uncompressed";
+          default-preference-list = "SHA512 SHA384 SHA256 AES256 AES192 AES ZLIB BZIP2 ZIP Uncompressed";
+          cert-digest-algo = "SHA512";
+          s2k-digest-algo = "SHA512";
+          s2k-cipher-algo = "AES256";
+          charset = "utf-8";
+          fixed-list-mode = true;
+          no-comments = true;
+          no-emit-version = true;
+          keyid-format = "0xlong";
+          list-options = "show-uid-validity";
+          verify-options = "show-uid-validity";
+          with-fingerprint = true;
+          require-cross-certification = true;
+          no-symkey-cache = true;
+          use-agent = true;
+          throw-keyids = true;
+        };
       };
     }
   ];
--- systems/lapnix-daily/security.nix
+++ systems/lapnix-daily/security.nix
@@ -1,11 +1,25 @@
 # nixos-config/systems/lapnix-daily/security.nix
 # Configure local and remote access for system 'daily' on host 'lapnix'.
-{ ... }:
+{ config, ... }:
 {
-  security.pam.services = {
-    login.u2fAuth = true;
-    sudo.u2fAuth = true;
+  security.pam = {
+    u2f = {
+      enable = true;
+      settings = {
+        origin = "pam://mp222";
+        authfile = config.sops.secrets."u2f_keys".path;
+        cue = true;
+      };
+    };
+    services = {
+      login.u2fAuth = true;
+      sudo.u2fAuth = true;
+    };
   };
+
+  # u2f keys are configured on the system level, so they shouldn't be owner by any specific user.
+  # Make u2f keys readable by everyone, so user level things, like hyprlock, can also read them.
+  sops.secrets."u2f_keys".mode = "0444";
 
   services.openssh = {
     enable = true;
