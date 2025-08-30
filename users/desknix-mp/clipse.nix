87c52a3fcb3dca052bb6c0fa60aa8534b37362a1
Mikko Pitkänen <mikko.pitkanen.code@pm.me>
2025-08-31 00:14:19 +0300

    feat: add clipse theming

--- users/desknix-mp/clipse.nix
+++ users/desknix-mp/clipse.nix
@@ -6,17 +6,43 @@
   home-manager.users.mp = {
     home.packages = [ pkgs.wl-clipboard ];
 
-    wayland.windowManager.hyprland.settings = {
-      exec-once = [ "clipse -listen" ];
-      windowrulev2 = [
-        "float,class:(clipse)"
-        "size 622 652,class:(clipse)"
-      ];
-    };
+    # No need to do `clipse -listen` manually on login / in Hyprland config.
+    # The HM module comes with a systemd unit `clipse.service`.
+    wayland.windowManager.hyprland.settings.windowrulev2 = [
+      "float,class:(clipse)"
+      "size 624 702,class:(clipse)"
+    ];
 
     services.clipse = {
       enable = true;
       imageDisplay.type = "kitty";
+      theme = {
+        useCustomTheme = true;
+        TitleFore = "#848484";
+        TitleBack = "#00000000";
+        TitleInfo = "#585858";
+        FilterPrompt = "#848484";
+        FilterInfo = "#585858";
+        FilterText = "#ffffff";
+        FilterCursor = "#848484";
+        NormalTitle = "#d0a028";
+        NormalDesc = "#54157e";
+        DimmedTitle = "#685014";
+        DimmedDesc = "#2a0a3f";
+        SelectedTitle = "#d39c87";
+        SelectedDesc = "#975da3";
+        SelectedBorder = "#683030";
+        SelectedDescBorder = "#683030";
+        StatusMsg = "#d39c87";
+        PinIndicatorColor = "#683030";
+        HelpKey = "#826419";
+        HelpDesc = "#35044f";
+        DividerDot = "#00000000";
+        PageActiveDot = "#d0a028";
+        PageInactiveDot = "#54157e";
+        PreviewedText = "#ffffff";
+        PreviewBorder = "#683030";
+      };
     };
   };
 }
