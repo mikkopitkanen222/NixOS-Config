8277eef6d462966806fa290aff93b63b63f5449c
Mikko Pitkänen <mikko.pitkanen.code@pm.me>
2025-08-30 16:35:17 +0300

    feat: remove protondrive
    Protondrive works great on rclone, at least until the system is
    upgraded.  After upgrading however, protondrive starts returning
    "invalid credentials" errors and the whole logging in process must be
    performed all over again.  This is fucking annoying and breaks the
    whole point of declarative setups.  Not to mention using rclone might be
    potentially hazardous to the wellbeing of Proton accounts, seeing that
    Proton does not support / is actively discouraging the use of rclone.
    
    Move to using the web app, until a better alternative is found.

--- users/desknix-mp/proton.nix
+++ users/desknix-mp/proton.nix
@@ -1,11 +1,6 @@
 # nixos-config/users/desknix-mp/proton.nix
 # Configure Proton software suite for user 'mp' on host 'desknix'.
-{
-  config,
-  inputs,
-  pkgs,
-  ...
-}:
+{ pkgs, ... }:
 {
   home-manager.users.mp = {
     home.packages = with pkgs; [
@@ -15,67 +10,19 @@
 
     services.protonmail-bridge.enable = true;
 
-    programs.rclone = {
-      enable = true;
-      remotes.proton = {
-        config = {
-          type = "protondrive";
-          enable_caching = false;
-        };
-        mounts."" = {
-          enable = true;
-          mountPoint = "/persist/proton";
-          options = {
-            dir-cache-time = "732h";
-            dir-perms = "700";
-            file-perms = "600";
-            link-perms = "600";
-            vfs-cache-mode = "writes";
-          };
-        };
-        secrets = {
-          username = config.sops.secrets."protondrive/username".path;
-          password = config.sops.secrets."protondrive/password".path;
-          "2fa" = config.sops.secrets."protondrive/2fa".path;
-          client_uid = config.sops.secrets."protondrive/client_uid".path;
-          client_access_token =
-            config.sops.secrets."protondrive/client_access_token".path;
-          client_refresh_token =
-            config.sops.secrets."protondrive/client_refresh_token".path;
-          client_salted_key_pass =
-            config.sops.secrets."protondrive/client_salted_key_pass".path;
-        };
-      };
-    };
-
-    systemd.user.services."rclone-mount:@proton" = {
-      # Stop trying to mount proton: after a couple failed tries.
-      # Otherwise 10 attempts in a row are fired off right at boot,
-      # causing invalidation of saved protondrive credentials.
-      Unit = {
-        StartLimitIntervalSec = 150;
-        StartLimitBurst = 2;
-      };
-      Service = {
-        RestartSec = 60;
-      };
-    };
+    # Removed rclone config for protondrive. Use the web app until such time
+    # that linux is officially supported or a better alternative presents itself.
+    # TODO: See if the android app is at all faster to use and if it can be used
+    # from the command line.
+    #
+    # Using Protondrive with Rclone works, but after every system upgrade I get
+    # - "Invalid access token",
+    # - "Invalid refresh token", and
+    # - "Incorrect login credentials"
+    # and have to redo `rclone config`. Logging in repeatedly with "invalid"
+    # credentials gets my account temporarily blocked, which is worrying.
+    # Discussions on Reddit and Discourse have quotes from Proton customer
+    # support stating that rclone is not supported and suggesting people should
+    # stop using it, if they wish to continue using the service.
   };
-
-  sops.secrets =
-    let
-      proton-sops = {
-        sopsFile = "${inputs.nixos-secrets}/desknix-proton.yaml";
-        owner = config.users.users.mp.name;
-      };
-    in
-    {
-      "protondrive/username" = proton-sops;
-      "protondrive/password" = proton-sops;
-      "protondrive/2fa" = proton-sops;
-      "protondrive/client_uid" = proton-sops;
-      "protondrive/client_access_token" = proton-sops;
-      "protondrive/client_refresh_token" = proton-sops;
-      "protondrive/client_salted_key_pass" = proton-sops;
-    };
 }
