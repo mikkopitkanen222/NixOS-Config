91b7049d48a7050ce38b1b3e69139e93ac1ad0ba
Mikko Pitkänen <mikko.pitkanen.code@pm.me>
2025-12-31 05:22:24 +0200

    feat: activate numlock in boot stage 1

--- systems/desknix/host-configuration.nix
+++ systems/desknix/host-configuration.nix
@@ -14,6 +14,23 @@
     efi.canTouchEfiVariables = true;
   };
 
+  boot.initrd.systemd = {
+    initrdBin = [ pkgs.kbd ];
+    services."activateNumlock" = {
+      description = "Activate numlock before prompting for a LUKS decryption passphrase.";
+      wantedBy = [ "initrd.target" ];
+      before = [ "sysinit.target" ];
+      unitConfig.DefaultDependencies = "no";
+      serviceConfig.Type = "oneshot";
+      script = ''
+        INITTY=/dev/tty[1-6]
+        for tty in $INITTY; do
+          /bin/setleds -D +num < $tty
+        done
+      '';
+    };
+  };
+
   hardware = {
     enableRedistributableFirmware = true;
     keyboard.qmk.enable = true;
