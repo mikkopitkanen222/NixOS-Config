d53df54a9320b54ffc8515f2bc03e5c1b06572e6
Mikko Pitkänen <mikko.pitkanen.code@pm.me>
2026-05-23 15:49:46 +0300

    chore: update flake
    Patch bluetooth. Remove patch when kernel 7.0.10 hits.

--- systems/desknix/host-configuration.nix
+++ systems/desknix/host-configuration.nix
@@ -5,6 +5,16 @@
   system.stateVersion = "25.05";
   boot.kernelPackages = pkgs.linuxPackages_latest;
 
+  boot.kernelPatches = [
+    {
+      name = "Bluetooth: btmtk: accept too short WMT FUNC_CTRL events";
+      patch = pkgs.fetchurl {
+        url = "https://git.kernel.org/pub/scm/linux/kernel/git/bluetooth/bluetooth-next.git/patch/?id=162b1adeb057d28ad84fd8a03f3c50cf08db5c62";
+        hash = "sha256-3iG77qRobyeMELrPR/khzWDJ4DpvUpr5PIghFPAMVoU=";
+      };
+    }
+  ];
+
   boot.loader = {
     timeout = 0;
     systemd-boot = {
